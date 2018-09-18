a953719325a3977043f166eaf7a74c19267e146f
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-18 21:38:02 +0200

    Add new TypeConversion project.

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit conversion
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            // Explicit cast
+            int second = 1000;
+            byte bSecond = (byte)second;
+            Console.WriteLine(bSecond);
+
+            string number = "1234";
+            int convertNumber = Convert.ToInt32(number);
+            Console.WriteLine(convertNumber);
+
+            string boolean = "true";
+            bool isResult = Convert.ToBoolean(boolean);
+            Console.WriteLine(isResult);
+
+            Console.ReadLine();
+        }
+    }
+}

b5a941377848aba3d179062c6350ed9fc5ae22e0
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-20 22:34:06 +0200

    Add EnumsDemo project.

---
+++ EnumsDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsDemo
+{
+    // We must define enum at the namespace level because it is a new type
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method); // Result is 3
+
+            int methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId); // Result is Express
+
+            Console.WriteLine(method.ToString()); // Result is Express
+
+            // Convert string to enum
+            var methodName = "Express";
+            var convertMethodName = (ShippingMethod)Enum.Parse(enumType: typeof(ShippingMethod), value: methodName);
+            Console.WriteLine(convertMethodName);
+
+            Console.ReadLine();
+        }
+    }
+}

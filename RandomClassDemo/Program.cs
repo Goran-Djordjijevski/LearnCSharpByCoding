d52fb19c3917317dcfb6912700c8380eb169b301
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-24 22:54:18 +0200

    Add RandomClassDemo project.

---
+++ RandomClassDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var randomNumber = random.Next(minValue: 97, maxValue: 122);
+                Console.Write((char)randomNumber );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

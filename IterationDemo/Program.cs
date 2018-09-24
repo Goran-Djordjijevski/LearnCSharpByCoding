413d078c1f29c721fb9d8b2a0fce671a2d7a7cbb
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-24 22:25:00 +0200

    Updated IterationDemo project.

--- IterationDemo/Program.cs
+++ IterationDemo/Program.cs
@@ -10,12 +10,59 @@ namespace IterationDemo
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 10; i++)
+            // For loop
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("-------------------");
+
+            //// Foreach loop
+            //string name = "John Smith";
+            //foreach (var c in name)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            //Console.WriteLine("-------------------");
+
+            //int[] numbers = { 1, 2, 3, 4 };
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine("-------------------");
+
+            //// While loops
+            //int i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //    i++;
+            //}
+
+            Console.WriteLine("-------------------");
+
+            while (true)
             {
-                if (i % 2 == 0)
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine(i);
+                    break;
                 }
+
+                Console.WriteLine($"@Echo: {input}");
             }
 
             Console.ReadLine();

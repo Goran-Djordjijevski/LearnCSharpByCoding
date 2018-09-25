ec1051f194a406c3431ac26e6f00ce32826685a1
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-25 21:22:58 +0200

    Update IterationExercises project.

--- IterationExercises/Program.cs
+++ IterationExercises/Program.cs
@@ -24,7 +24,33 @@ namespace IterationExercises
 
             //ExerciseThree();
 
+            //4 - Write a program that picks a random number between 1 and 10.Give the user 4 chances to guess the number.If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+            //ExampleFour();
+
             Console.ReadLine();
+        }
+
+        private static void ExampleFour()
+        {
+            Console.WriteLine("Random number game");
+
+            var random = new Random();
+            var number = random.Next(minValue: 1, maxValue: 10);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Pick a number: ");
+                var input = Convert.ToInt32(Console.ReadLine());
+
+                if (number == input)
+                {
+                    Console.WriteLine("You won!");
+                    break;
+                }
+
+                Console.WriteLine("You lost!");
+            }
         }
 
         private static void ExerciseThree()

e161f41b2c90c049ec60a1ebf46ad25966660555
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-25 23:07:31 +0200

    Add DemoList project.

---
+++ DemoLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+            numbers.Add(5);
+            foreach (var item in numbers)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            numbers.AddRange(collection: new List<int> { 6, 7, 8 });
+            foreach (var item in numbers)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Index of 4: {numbers.IndexOf(item: 4)}");
+
+            Console.WriteLine($"Count: {numbers.Count}");
+
+            var remove = numbers.Remove(5);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

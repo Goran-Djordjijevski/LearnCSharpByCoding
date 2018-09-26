49b427ecdd0117c15dc575b833d30ee01bd5493e
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-26 21:36:22 +0200

    Update DemoList project.

--- ListsDemo/Program.cs
+++ ListsDemo/Program.cs
@@ -19,9 +19,82 @@ namespace ListsDemo
 
             //Write a program and continuously ask the user to enter different names, until the user presses Enter(without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
 
-            ExerciseOne();
+            //ExerciseOne();
+
+            //2 - Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string.Display the reversed name on the console.
+
+            //ExerciseTwo();
+
+            //3 - Write a program and ask the user to enter 5 numbers.If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+            //ExerciseThree();
+
+            //4 - Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates.Display the unique numbers that the user has entered.
+
+            //ExerciseFour();
 
             Console.ReadLine();
+        }
+
+        private static void ExerciseFour()
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number or type QUIT to exit from app: ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                var number = Convert.ToInt32(input);
+                numbers.Add(number);
+            }
+
+            var distinctValue = numbers.Distinct();
+            foreach (var number in distinctValue)
+            {
+                Console.Write(number);
+            }
+        }
+
+        private static void ExerciseThree()
+        {
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter a number: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("You've previously entered " + number);
+                    continue;
+                }
+
+                numbers.Add(number);
+            }
+
+            numbers.Sort();
+
+            foreach (var number in numbers)
+                Console.Write(number);
+        }
+
+        private static void ExerciseTwo()
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
         }
 
         private static void ExerciseOne()

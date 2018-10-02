be8de6370c4d15da863611b44b117e8778545124
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-02 19:21:17 +0200

    TimeSpanDemo project.

---
+++ TimeSpanDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeSpan = new TimeSpan(hours: 1, minutes: 2, seconds: 3);
+
+
+            Console.ReadLine();
+        }
+    }
+}

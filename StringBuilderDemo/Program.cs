b345f2d0339b8f587a688b9e0b484742405348de
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-10 23:03:38 +0200

    Add StringBuilderDemo project.

---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+            var append = builder.Append('-', 10);
+            append.AppendLine();
+            append.Append("Header");
+            append.AppendLine();
+            append.Append('-', 10);
+
+            builder.Replace('-', '+');
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(append);
+
+            Console.ReadLine();
+        }
+    }
+}

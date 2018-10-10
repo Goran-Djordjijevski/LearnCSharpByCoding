4171336954620ffe6500663cb1242353a1d6cbe9
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-10 20:16:36 +0200

    Add TimeSpanDemo project.

--- TimeSpanDemo/Program.cs
+++ TimeSpanDemo/Program.cs
@@ -10,8 +10,29 @@ namespace TimeSpanDemo
     {
         static void Main(string[] args)
         {
+            // Creating
             var timeSpan = new TimeSpan(hours: 1, minutes: 2, seconds: 3);
 
+            // Second way to declare TimeSpan
+            var fromHours = TimeSpan.FromHours(value: 14);
+            Console.WriteLine(fromHours);
+
+            // If we subtract two DateTime we get TimeSpan
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(15);
+            var duration = end - start;
+            Console.WriteLine($"Duration: {duration}");
+
+            // Properties
+            Console.WriteLine($"Minutes: {timeSpan.Minutes}");
+            Console.WriteLine($"Total Minutes: {timeSpan.TotalMinutes}");
+
+            // Add
+            Console.WriteLine($"Add example: {timeSpan.Add(TimeSpan.FromMinutes(8))}");
+            Console.WriteLine($"Subtract example: {timeSpan.Add(TimeSpan.FromMinutes(2))}");
+
+            // ToString()
+            Console.WriteLine($"ToString: {timeSpan.ToString()}");
 
             Console.ReadLine();
         }

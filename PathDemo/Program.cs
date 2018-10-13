9caa19d863d1d05f146da7bb1a8ddbf3846a7fc7
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-13 20:55:58 +0200

    Add PathDemo project.

---
+++ PathDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\goran\Documents\GitHub\LearnCSharpByCoding\PathDemo.sln";
+
+            string extension = Path.GetExtension(path);
+            Console.WriteLine($"Extension: {extension}");
+
+            Console.WriteLine($"File name: {Path.GetFileName(path)}");
+            Console.WriteLine($"File name without extension: {Path.GetFileNameWithoutExtension(path)}");
+            Console.WriteLine($"Directory name: {Path.GetDirectoryName(path)}");
+
+            Console.ReadLine();
+        }
+    }
+}

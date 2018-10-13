a6e9899842e1209cf8d22bbf6d308eb304ab0572
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-13 15:30:20 +0200

    Add FileAndFileInfoDemo project.

---
+++ FileAndFileInfoDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndFileInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(sourceFileName: @"c:\temp\myfile.jpg", destFileName: @"d:\temp\myfile.jpg", overwrite: true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

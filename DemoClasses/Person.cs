794191bc6f4d1b6d52c52d580fbe247dcf8ec508
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-16 22:06:58 +0200

    Add DemoClasses project.

---
+++ DemoClasses/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {this.Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
---
+++ DemoClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Goran";
+            person.Introduce("Pixi");
+
+            Console.ReadLine();
+        }
+    }
+}

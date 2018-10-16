d841de69263a12fe8be02f8603f770b428ba055f
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-16 23:02:35 +0200

    Add ConstructorsDemo project.

---
+++ ConstructorsDemo/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            this.Orders = new List<Order>();
+        }
+
+        public Customer(int id) : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}

f52e545dddf06ee311604234515e51d6d21d8d57
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-10-18 20:55:55 +0200

    Add MethodsDemo project.

--- MethodsDemo/Point.cs
+++ MethodsDemo/Point.cs
@@ -16,5 +16,21 @@ namespace MethodsDemo
             this.X = x;
             this.Y = y;
         }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+            {
+                throw new ArgumentNullException("newLocation");
+            }
+
+            Move(newLocation.X, newLocation.Y);
+        }
     }
 }
--- MethodsDemo/Program.cs
+++ MethodsDemo/Program.cs
@@ -10,9 +10,45 @@ namespace MethodsDemo
     {
         static void Main(string[] args)
         {
+            //var number = int.Parse("abc");
 
+            int number;
+            var convert = int.TryParse("abc", out number);
+            if (convert == true)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed.");
+            }
 
             Console.ReadLine();
+        }
+
+        public static void UseParams()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Add(1, 2, 3, 4);
+            Console.WriteLine(result);
+        }
+
+        public static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+
+                point.Move(new Point(40, 60));
+                Console.WriteLine($"Point is at {point.X} and {point.Y}");
+
+                point.Move(100, 200);
+                Console.WriteLine($"Point is at {point.X} and {point.Y}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }

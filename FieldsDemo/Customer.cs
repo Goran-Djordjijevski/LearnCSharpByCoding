f16d6b4e756706ca9eb2300467c2d6018a442a76
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-16 20:59:44 +0100

    Add new FieldsDemo project.

---
+++ FieldsDemo/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            Orders = new List<Order>();
+            // .... 
+        }
+    }
+}

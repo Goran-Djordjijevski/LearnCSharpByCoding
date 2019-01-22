27adc29cb32d7eb71229b0020add49efae956d1f
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-22 20:37:34 +0100

    Added new project AccessModifiersDemo

---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(year: 1987, month: 8, day: 23));
+            Console.WriteLine(person.GetBirthdate());
+
+            Console.ReadLine();
+        }
+    }
+}

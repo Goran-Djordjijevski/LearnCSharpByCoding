9778fb9dfd9064b879ea30d484eda64efaa08704
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-22 20:55:13 +0100

    Add new project PropertiesDemo.

---
+++ PropertiesDemo/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; private set; }
+        public string MyProperty { get; set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                 
+                return years;
+            }
+            set { _age = value; }
+        }
+    }
+}
---
+++ PropertiesDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1987, 8, 23));
+            Console.WriteLine(person.Age);
+
+            Console.ReadLine();
+        }
+    }
+}

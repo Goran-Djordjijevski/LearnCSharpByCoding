0d1d8b1ed6a6cb6e1acf31b85327b8895a72a255
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-22 21:20:40 +0100

    Add new project IndexersDemo

---
+++ IndexersDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Goran";
+            Console.WriteLine(cookie["name"]);
+
+            Console.ReadLine();
+        }
+    }
+}

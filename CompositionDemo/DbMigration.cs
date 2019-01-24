16c19a414bf3b5bda116be883d21fe6c4b48785c
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-24 21:04:55 +0100

    Add new CompositionDemo project.

---
+++ CompositionDemo/DbMigration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionDemo
+{
+    public class DbMigration
+    {
+        private readonly Logger _logger;
+
+        public DbMigration(Logger logger)
+        {
+            this._logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migration some stuff...");
+        }
+    }
+}
---
+++ CompositionDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigration(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
+
+            Console.ReadLine();
+        }
+    }
+}

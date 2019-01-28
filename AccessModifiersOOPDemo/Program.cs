a11f1ef86ff392cdfd9a656ad925a04adb0d7869
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-28 19:52:00 +0100

    Add new project AccessModifiesOOPDemo.

---
+++ AccessModifiersOOPDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Amazon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersOOPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+
+            Console.ReadLine();
+        }
+    }
+}

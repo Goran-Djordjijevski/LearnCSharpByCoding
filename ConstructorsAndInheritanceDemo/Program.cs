12446cde08e5829ca9d006d25aab6498b9a7af6c
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-28 20:42:37 +0100

    Add new project ConstructorsAndInheritanceDemo

---
+++ ConstructorsAndInheritanceDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndInheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("12345");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorsAndInheritanceDemo/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndInheritanceDemo
+{
+    public class Vehicle
+    {
+        private readonly string registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized.");
+        //}
+
+        public Vehicle(string registrationNumber)
+        {
+            this.registrationNumber = registrationNumber;
+
+            Console.WriteLine($"Vehicle is being initialized. {registrationNumber}");
+        }
+    }
+}

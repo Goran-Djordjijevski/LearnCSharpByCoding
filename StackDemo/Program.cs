7a0c2e522c80b40f2bb535cc44359adc647307c1
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-01-30 22:02:29 +0100

    Add new Exercise StackObj.

---
+++ StackDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackObj();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackDemo/StackObj.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDemo
+{
+    public class StackObj
+    {
+        public List<object> stacks = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("You pass null, please pass valid input.");
+            }
+
+            stacks.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stacks.Count == 0)
+            {
+                throw new InvalidOperationException("Your stack is empty.");
+            }
+
+            var lastValue = stacks.LastOrDefault();
+            stacks.Remove(lastValue);
+
+            return lastValue;
+        }
+
+        public void Clear()
+        {
+            stacks.Clear();
+        }
+    }
+}

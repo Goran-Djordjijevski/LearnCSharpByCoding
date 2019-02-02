f5941f460bd5fc5e9b328025e6b61c508e623ab0
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2019-02-02 20:07:09 +0100

    Add new project.

---
+++ InterfacesAndExtensibility/DbMigrator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndExtensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo($"Migration started at {DateTime.Now}");
+
+            // Details of migrating the database
+
+            _logger.LogInfo($"Migration finished at {DateTime.Now}");
+        }
+    }
+}
